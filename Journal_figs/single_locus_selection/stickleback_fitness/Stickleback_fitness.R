066bb8d33eaa6c3ebd7b6a7911d95951057f230c
James Kitchens <40303683+kitchensjn@users.noreply.github.com>
2022-10-06 15:55:39 -0700

    Retry Pages Deployment

--- Journal_figs/single_locus_selection/stickleback_fitness/Stickleback_fitness.R
+++
@@ -1,9 +0,0 @@
-
-
-stickleback<-read.csv("Journal_figs/single_locus_selection/stickleback_fitness/Stickleback_fitness.csv")
-
-stickleback[4,c("CC","LC","LL")]
-stickleback[10,c("CC","LC","LL")]
-
-
-stickleback[10,c("CC","LC","LL")]/stickleback[4,c("CC","LC","LL")]/2.28
--- README.md
+++
@@ -1,1 +0,0 @@
-# popgen-notes-jb
